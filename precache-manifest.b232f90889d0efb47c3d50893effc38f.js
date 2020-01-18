6fedc448498ba679d0ada5a59d56136c83c33975
kwonkijun <kkj6369@naver.com>
2020-01-18 21:55:28 +0900

    Updates

---
+++ precache-manifest.b232f90889d0efb47c3d50893effc38f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8196ff5886fff4c7d905813ac1fd001",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "d7a0b865eac52cc724c4",
+    "url": "/movie_app_2020/static/css/main.ae4cfe19.chunk.css"
+  },
+  {
+    "revision": "35cc5f30209bbe41aed2",
+    "url": "/movie_app_2020/static/js/2.c595e5ea.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/movie_app_2020/static/js/2.c595e5ea.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d7a0b865eac52cc724c4",
+    "url": "/movie_app_2020/static/js/main.bbfb58e1.chunk.js"
+  },
+  {
+    "revision": "01aae0007e1cfd873da8",
+    "url": "/movie_app_2020/static/js/runtime-main.482f7179.js"
+  }
+]);
